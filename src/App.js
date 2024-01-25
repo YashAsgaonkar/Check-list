a2d96b0aad449415a77e58cc9beccdc658b837cc
YashAsgaonkar <yash240904@gmail.com>
2024-01-25 13:40:19 +0530

    Files uploaded to hub

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import "./index.css";
+import { useState } from "react";
+import { Logo } from "./components/Logo";
+import { Form } from "./components/Form";
+import PackingList from "./components/PackingList";
+import { Stats } from "./components/Stats";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  const handleAddItems = (item) => {
+    setItems((items) => [...items, item]);
+  };
+
+  const handleDeleteItem = (id) => {
+    setItems((items) => items.filter((item) => item.id !== id));
+  };
+
+  const handleToggleItem = (id) => {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  };
+
+  const handleClearList = () => {
+    alert("Are you sure you want to delete all items?");
+    setItems([]);
+  };
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
